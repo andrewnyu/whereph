2d870882e9fa90be56925069924fa5c62a02892e
Andrew Yu <andrewthomasyu@gmail.com>
2022-01-13 21:56:33 +0800

    Modify batch spatial join to allow custom join method 1.) Allow custom join_method input 2.) Set default to 'right'
    We want this behavior to keep user uploads where GID is not found in
    export with GID information left blank.

--- app/scripts/whereph.py
+++ app/scripts/whereph.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 
-def simple_sjoin(df, shapes):
+def simple_sjoin(df, shapes, join_method='right'):
     #convert to GeoDataFrame and add CRS 4326
     df = gpd.GeoDataFrame(
         df, geometry=gpd.points_from_xy(df['longitude'], df['latitude']))
     df.set_crs(epsg=4326, inplace=True)
     
-    df_joined = gpd.sjoin(shapes, df)
+    df_joined = gpd.sjoin(shapes, df, how=join_method)
     return df_joined
 
 def sjoin_point(lat, lon, shapes):
