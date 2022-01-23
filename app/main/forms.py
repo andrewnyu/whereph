651eba5a5bf08e2f0c3b3604349d2a02e895ebb9
Andrew Yu <andrewthomasyu@gmail.com>
2022-01-23 15:04:19 +0800

    Remove unnecessary imports

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,5 @@
-from flask import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
-from wtforms.validators import ValidationError, DataRequired
 
 class PointSjoinForm(FlaskForm):  
     latitude = StringField(u'Latitude')
